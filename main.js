59188544611b445b4cb00179a8122f6f0822798b
Rushikesh Chincholkar <114005115+RushiCoder@users.noreply.github.com>
2023-08-18 21:18:51 +0530

    Add files via upload
    This is a Gamicbook made by using Html , Css and JavaScript

---
+++ main.js
@@ -0,0 +1,55 @@
+// Cross icon move 
+let menu = document.querySelector('.menu-icon');
+let navbar = document.querySelector('.menu');
+
+menu.onclick = () => {
+    navbar.classList.toggle('active');
+    menu.classList.toggle('move');
+    bell.classList.remove('active');
+}
+
+// Notification
+let bell = document.querySelector('.notification');
+
+document.querySelector('#bell-icon').onclick = () => {
+    bell.classList.toggle('active');
+}
+
+
+// Swiper
+var swiper = new Swiper(".trending-content", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+      delay:3000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 15,
+      },
+      1068: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+    },
+  });
+
+  // Custom Scroll Bar 
+  window.onscroll = function() {mufunction()};
+
+  function mufunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById('scroll-bar').style.width = scrolled + '%';
+  }
